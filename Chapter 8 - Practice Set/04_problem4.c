980292af487e8b527f5f8ed4aa826f39a6baade1
Aurelius <aurelius@example.com>
2026-06-27 11:23:25 +0530

    Fix Chapter 8 practice set problem 4 slicing logic

--- Chapter 8 - Practice Set/04_problem4.c
+++ Chapter 8 - Practice Set/04_problem4.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-
 char* slice(char str[], int m, int n){
-    str = &str[m];
-    str[n-m+1] = '\0';
+    char *ptr = &str[m];
+    ptr[n - m + 1] = '\0';
 
-    return str;
+    return ptr;
 }
+
 int main(){
-    char str[] = "Harry bhai";
-    
-    printf("%s", slice(str, 1, 7));
+    char str[] = "Himanshu Srivastav";
+
+    printf("%s\n", slice(str, 6, 11)); // Output: hu Sri
     return 0;
 }
